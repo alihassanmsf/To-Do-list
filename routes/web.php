a3a6f2f38b14502e44b17321f22b9aa5c9455b5c
Ali Hassan <al.m1420@yahoo.com>
2025-01-21 11:00:09 +0300

    Adding routes for login, register and logout also for task

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Route::middleware(['auth', 'role:admin'])->group(function () {
+//    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+//});
+
+
+Route::resource('task', TaskController::class);
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
