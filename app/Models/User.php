9dcb05f88a8433d1a0cac299c49118f25ca78b37
Ali Hassan <al.m1420@yahoo.com>
2025-01-23 11:31:19 +0300

    Add role_id fillable and create function new function to get role name

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -46,4 +47,10 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class, 'role_id');
     }
+
+    public function hasRole($role)
+    {
+        return $this->role?->name === $role;
+    }
+
 }
