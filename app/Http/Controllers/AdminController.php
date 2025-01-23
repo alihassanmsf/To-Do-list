39eb387d900ea19d54de058cb7853274fa4d5c15
Ali Hassan <al.m1420@yahoo.com>
2025-01-23 11:37:07 +0300

    Create Admin Controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        $totalUsers = User::all()->count();
+        $totalTasks = Task::all()->count();
+        $totalPandingTasks = Task::where('status', 'pending')->count();
+        $totalCompletedTasks = Task::where('status', 'closed')->count();
+        $totalOpenedTasks = Task::where('status', 'open')->count();
+
+
+        return view('admin.index',['totalUsers'=>$totalUsers,'totalTasks'=>$totalTasks,
+            'totalPandingTasks'=>$totalPandingTasks,'totalCompletedTasks'=>$totalCompletedTasks,
+            'totalOpenedTasks'=>$totalOpenedTasks]);
+    }
+}
