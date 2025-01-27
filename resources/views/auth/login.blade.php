e67189cd24d61206b3c5e4fc1cd25c1dbfec6bb4
Ali Hassan <devalihassan324@gmail.com>
2025-01-27 12:51:20 +0300

    Use layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,11 @@
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Login</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+
+@endsection
