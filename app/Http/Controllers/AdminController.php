74ed14bf43c35065f797ff06fc0ed10a1882d8ea
Ali Hassan <devalihassan324@gmail.com>
2025-01-30 09:33:02 +0300

    Create functions to create and list users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -20,5 +22,37 @@ class AdminController extends Controller
         return view('admin.index',['totalUsers'=>$totalUsers,'totalTasks'=>$totalTasks,
             'totalPandingTasks'=>$totalPandingTasks,'totalCompletedTasks'=>$totalCompletedTasks,
             'totalOpenedTasks'=>$totalOpenedTasks]);
+    }
+
+    public function usersList(Request $request)
+    {
+        $users = User::all();
+        return view('admin.users.users-list',['users'=>$users]);
+    }
+
+    public function createUser(Request $request)
+    {
+        $roles = Role::all();
+        return view('admin.create-user',['roles'=>$roles]);
+    }
+
+    public function storeUser(Request $request)
+    {
+         $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role_id' => 'required|exists:roles,id',
+        ]);
+
+        User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password' => Hash::make($request->passeord),
+            'role_id' => $request->role_id
+
+        ]);
+
+        return redirect()->route('admin.users.index')->with('success', 'User created successfully.');
     }
 }
