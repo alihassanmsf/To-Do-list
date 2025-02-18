529721f112e38842156c2d386152d8d4709617e1
Ali Hassan <al.m1420@yahoo.com>
2025-02-18 10:30:55 +0300

    Query the activity table to display in dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use App\Models\Role;
 use App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
@@ -16,43 +18,18 @@ class AdminController extends Controller
         $totalTasks = Task::all()->count();
         $totalPandingTasks = Task::where('status', 'pending')->count();
         $totalCompletedTasks = Task::where('status', 'completed')->count();
-        $totalOpenedTasks = Task::where('status', 'open')->count();
+        $totalInProgressTasks = Task::where('status', 'in_progress')->count();
+        $activities = Activity::with('user', 'task')->latest()->take(10)->get();
 
 
-        return view('admin.index',['totalUsers'=>$totalUsers,'totalTasks'=>$totalTasks,
-            'totalPandingTasks'=>$totalPandingTasks,'totalCompletedTasks'=>$totalCompletedTasks,
-            'totalOpenedTasks'=>$totalOpenedTasks]);
+        return view('admin.index', ['totalUsers' => $totalUsers, 'totalTasks' => $totalTasks,
+            'totalPandingTasks' => $totalPandingTasks, 'totalCompletedTasks' => $totalCompletedTasks,
+            'totalInProgressTasks' => $totalInProgressTasks, 'activities' => $activities]);
     }
 
     public function usersList(Request $request)
     {
         $users = User::all();
-        return view('admin.users.users-list',['users'=>$users]);
-    }
-
-    public function createUser(Request $request)
-    {
-        $roles = Role::all();
-        return view('admin.create-user',['roles'=>$roles]);
-    }
-
-    public function storeUser(Request $request)
-    {
-         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-            'role_id' => 'required|exists:roles,id',
-        ]);
-
-        User::create([
-            'name'=> $request->name,
-            'email'=> $request->email,
-            'password' => Hash::make($request->passeord),
-            'role_id' => $request->role_id
-
-        ]);
-
-        return redirect()->route('admin.users.index')->with('success', 'User created successfully.');
+        return view('admin.users.users-list', ['users' => $users]);
     }
 }
