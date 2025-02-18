58db1bf23cb342696b92c8e18518faa21f286a8a
Ali Hassan <al.m1420@yahoo.com>
2025-02-18 14:36:28 +0300

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ManagerController;
 use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,14 +30,40 @@ Route::middleware(['auth', 'role:Admin'])->group(function (){
     Route::get('/dashboard',[AdminController::class,'index'])->name('admin.dashboard');
     Route::get('/dashboard/users/list',[AdminController::class,'usersList'])->name('admin.users.list');
     Route::get('/dashboard/users/create-user',[AdminController::class,'createUser'])->name('admin.create-user');
+    Route::get('/dashboard/tasks/create-task',[TasksController::class,'create'])->name('admin.create-task');
     Route::post('/dashboard/users',[AdminController::class,'storeUser'])->name('admin.store-user');
+    Route::get('/dashboard/users/{user}/edit',[AdminController::class,'editUser'])->name('admin.edit-user');
+    Route::put('/dashboard/users/{user}',[AdminController::class,'updateUser'])->name('admin.update-user');
+    Route::get('/dashboard/manage-users', function () {
+        return view('admin.manage_users');
+    })->name('admin.manage-users');
+
+    Route::get('/dashboard/manage-tasks', function () {
+        return view('admin.manage_tasks');
+    })->name('admin.manage-tasks');
+
+});
+
+Route::middleware(['auth', 'role:Manager'])->group(function (){
+    Route::get('manager/dashboard',[ManagerController::class,'index'])->name('manager.dashboard');
+    Route::get('manager/dashboard/manage-tasks', function () {
+        return view('admin.manage_tasks');
+    })->name('manager.manage-tasks');
 });
 Route::resource('tasks', TasksController::class);
 
 Route::post('/tasks/{task}/complete', [TasksController::class, 'completeTask'])->name('tasks.complete');
+Route::post('/tasks/{task}/in_progress', [TasksController::class, 'triggerToInProgress'])->name('tasks.in_progress');
+
 ## Auth region
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('password/reset', [AuthController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [AuthController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [AuthController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [AuthController::class, 'reset'])->name('password.update');
+Route::get('/profile', [AuthController::class, 'showProfile'])->name('profile')->middleware('auth');
+Route::put('/profile/update', [AuthController::class, 'updateProfile'])->name('profile.update')->middleware('auth');
