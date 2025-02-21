47c53fec8db938c29cea94cab6ec8f234ab940ea
Ali Hassan <al.m1420@yahoo.com>
2025-02-21 17:07:10 +0300

    Enhance the logic

--- app/Livewire/TaskSearch.php
+++ app/Livewire/TaskSearch.php
@@ -2,18 +2,24 @@
 
 namespace App\Livewire;
 
+namespace App\Livewire;
+
 use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class TaskSearch extends Component
 {
-    public $search;
+    public $search = '';
+
+    protected $queryString = ['search'];
+
     public function render()
     {
-        $tasks  = Task::where([
-            ['name', 'like', '%' . $this->search . '%'],
-            ['assigned_for',Auth::id()]])->get();
-        return view('livewire.task-search', compact('tasks'));
+        return view('livewire.task-search', [
+            'tasks' => Task::where('name', 'like', '%' . $this->search . '%')
+                ->where('assigned_for', Auth::id())
+                ->get()
+        ]);
     }
 }
