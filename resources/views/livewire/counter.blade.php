6193658ff22ac6824b08cd82c3afb7337051f069
Ali Hassan <al.m1420@yahoo.com>
2025-02-24 23:31:01 +0300

    Install Alpine.js

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div class="p-4 border rounded">
+    <h1 class="text-2xl font-bold">Counter: {{ $count }}</h1>
+    <button wire:click="increment" class="px-4 py-2 bg-blue-500 text-white rounded">
+        Increment
+    </button>
+</div>
