2ea0170b6e084aaaec934b88ecccae3a1b739d85
Ali Hassan <al.m1420@yahoo.com>
2025-02-27 11:26:46 +0300

    Add imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.js",
-        "./resources/css/**/*.css",
-    ],
+    content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
     theme: {
         extend: {},
     },
     plugins: [],
-}
+};
