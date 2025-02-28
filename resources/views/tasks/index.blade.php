711f5514f9e7df7b770764b83fd2938dadc1dc8d
Ali Hassan <al.m1420@yahoo.com>
2025-02-28 21:53:25 +0300

    Adding the alert

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+    <!-- ✅ Success Alert -->
+    @if (session()->has('message'))
+        <div class="p-3 mb-4 rounded-lg bg-green-500 text-white shadow-lg">
+            {{ session('message') }}
+        </div>
+    @endif
     @livewire('task-search')
 @endsection
